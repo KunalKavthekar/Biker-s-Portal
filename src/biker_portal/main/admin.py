d481564595e4329707a3056c5a33247fd65af6e3
KunalKavthekar <42344635+KunalKavthekar@users.noreply.github.com>
2020-04-19 19:41:10 +0530

    Add files via upload

---
+++ src/biker_portal/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ToDoList, Item, Search
+# Register your models here.
+# admin.site.register(ToDoList)
+# admin.site.register(Item)
+# admin.site.register(Search)
